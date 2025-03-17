01e03052124efe1c24f9779b9472f5d4c8fa11e2
BolatMukashev <ya.ne.angel.kimi@gmail.com>
2025-03-17 15:53:04 +0500

    добавил данные бд

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ config = dotenv_values(".env")
 
 BOT_TOKEN = config.get("BOT_TOKEN")
 OPENAI_API_KEY = config.get("OPENAI_API_KEY")
+ADMIN_ID = config.get("ADMIN_ID")
+MONGO_USERNAME = config.get("MONGO_USERNAME")
+MONGO_PASSWORD = config.get("MONGO_PASSWORD")
